6d7d7d620124a22099351ce0d4a923bac6f4a507
Yoan Fourcade <yoanfourcade@gmail.com>
2017-06-27 17:41:46 +0200

    Try to extract landscape fragmentation indices from R

--- analyses.R
+++ analyses.R
@@ -8,15 +8,19 @@ library(visreg)
 library(MuMIn)
 
 ##### import CTI data and merge with site data #####
-sites <- rio::import(file = "../Data/Butterflies - Netherlands/Sites.xlsx", which = 1L)
-ndvi_data <- read.csv(paste0("../Data/NDVI/NDVI_NL/ndvi.points.sum.scv"))
+#load sites
+sites <- read.csv(file = "../Data/Butterflies - Netherlands/Sites_ETRS89_landcover.csv")
 lc_class <- rio::import(file = "../Landcover/clc_legend.xls", which = 1L)
+sites <- merge(sites, lc_class, by.x = "Landcover", by.y = "CLC_CODE")
+
+# site data
+ndvi_data <- read.csv("../Data/NDVI/NDVI_NL/ndvi.points.sum.scv")
+frag_data <- read.csv("../Data/Butterflies - Netherlands/landFrag.csv")
 
 cti_AB <- read.csv2("../Data/Butterflies - Netherlands/cti_site_year_abundance_2017-05-19.csv", dec = ".")
 cti_AB$Site <- as.factor(cti_AB$Site)
-cti_AB <- merge(cti_AB, ndvi_data)
+cti_AB <- merge(cti_AB, frag_data)
 cti_AB <- merge(cti_AB, sites)
-cti_AB <- merge(cti_AB, lc_class, by.x = "Landcover", by.y = "CLC_CODE")
 cti_AB_mean <- aggregate(cti ~ Year, cti_AB, mean)
 
 
@@ -68,3 +72,21 @@ p.stv_AB <- ggplot(data = pred.m_ltv_AB$fit, aes(x = Year, y = visregFit, color 
 
 # plot
 plot_grid(p.ltv_AB, p.stv_AB, ncol = 2)
+
+
+##### Effect of connectivity #####
+m_frag_AB <- lmer(cti ~ Year * total.area + LABEL2 + (1|Site), data = stdize(cti_AB, prefix = F), na.action = na.fail)
+summary(m_frag_AB)
+
+dredge(m_frag_AB, fixed = c("Year", "LABEL3"))
+
+# plot
+pred.m_frag_AB <- visreg(m_frag_AB, xvar = "Year", ylab = "CTI", scale = "response", 
+                        by = "edge.density", gg = T, breaks = 3, plot = F)
+ggplot(data = pred.m_frag_AB$fit, aes(x = Year, y = visregFit, color = as.factor(edge.density))) + 
+  geom_line() + 
+  scale_y_continuous(name = "CTI") +
+  theme(legend.position = c(0.8,0.1)) + 
+  scale_color_manual(name = "edge.density", labels = c("Low", "Medium", "High"), values = c("royalblue1", "palegreen3", "tomato2")) 
+
+
--- connectivity.R
+++ connectivity.R
@@ -1,10 +1,49 @@
+# load libraries
+library(raster)
+library(SDMTools)
+library(rgeos)
+
+#load sites
+sites <- read.csv(file = "../Data/Butterflies - Netherlands/Sites_ETRS89_landcover.csv")
+lc_class <- rio::import(file = "../Landcover/clc_legend.xls", which = 1L)
+sites <- merge(sites, lc_class, by.x = "Landcover", by.y = "CLC_CODE")
+
 #load landcover
 CLC_NL <- raster("//storage.slu.se/Home$/yofo0001/My Documents/Recherche/FORMAS project/Landcover/CLC_NL.tif")
+# reclassify by second-level categories
+reclassTable <- data.frame(from = lc_class[,1], to = as.numeric(as.factor(lc_class[,4])))
+CLC_NL_reclas <- reclassify(CLC_NL, reclassTable)
+
+#landuse stats at monitoring sites
+table(sites$LABEL2)
+
 
 #select land-use category
-CLC_NL_forest <- CLC_NL
-CLC_NL_forest[CLC_NL_forest %in% 23:25] <- 1
-CLC_NL_forest[!CLC_NL_forest %in% 1] <- 0
+# Forest
+LC <- 23:25
 
-# aggregate raster
-CLC_NL_forest <- aggregate(CLC_NL_forest, 4, max)
+#reclass raster into habitat and non-habitat
+CLC_NL_sel <- CLC_NL
+CLC_NL_sel[CLC_NL_sel %in% LC] <- 1
+CLC_NL_sel[!CLC_NL_sel %in% 1] <- 0
+
+#create buffers
+pts <- sites[!sites$LABEL1 %in% "Artificial surfaces",]
+
+landFrag <- c()
+for(i in c(1:nrow(pts))){
+  landPt <- reclassTable[reclassTable$from %in% pts[i,"GRID_CODE"],2]
+  
+  buf <- gBuffer(SpatialPoints(pts[i,3:4]), width = 3000)
+  cr <- crop(CLC_NL_reclas, extent(buf), snap="out")
+  fr <- rasterize(buf, cr)
+  lr <- mask(x=cr, mask=fr)
+  
+  stat <- ClassStat(lr)
+  stat <- stat[stat$class %in% landPt,]
+
+  landFrag <- rbind.data.frame(landFrag, cbind.data.frame(Site = pts[i,2], stat))
+}
+
+write.csv(landFrag, "../Data/Butterflies - Netherlands/landFrag.csv", row.names = F)
+
